4054bbc84aa48307081817d5ab4b08eba47c8a2b
gaur8126 <gaurlokesh1211@gmail.com>
2025-03-19 22:35:50 +0530

    first commmit with project structure and research directory

---
+++ src/phishing_detector/logging/logger.py
@@ -0,0 +1,11 @@
+import os 
+import logging 
+from datetime import datetime
+logging.basicConfig(level=logging.INFO)
+
+# LOG_FILE = f"{datetime.now().strftime()}.log"
+
+
+
+if __name__== "__main__":
+    logging.info("Hello Himnashu")
