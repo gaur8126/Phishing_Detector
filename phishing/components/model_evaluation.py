32b8caa5a2ea7cdeda60a27f2bd9345f0bfc1182
gaur8126 <gaurlokesh1211@gmail.com>
2025-04-20 21:41:09 +0530

    final commit

--- phishing/components/model_evaluation.py
+++ phishing/components/model_evaluation.py
@@ -1,14 +1,18 @@
 import os 
 import pandas as pd 
-from sklearn.metrics import accuracy_score,confusion_matrix
+from sklearn.metrics import accuracy_score,f1_score,precision_score
 import numpy as  np 
 import joblib 
+import mlflow
 from pathlib import Path
 from phishing.utils.common import save_json
+from xgboost import XGBClassifier
 from phishing.logging.custom_logger import logging
 from phishing.exception.exception import PhishingException
-from phishing.config.configuration import ModelEvaluationConfig
+from phishing.config.configuration import ModelEvaluationConfig,ConfigurationManager
 
+import dagshub
+dagshub.init(repo_owner='gaur8126', repo_name='Phishing_Detector', mlflow=True)
 
 class ModelEvaluation:
     def __init__(self, config: ModelEvaluationConfig):
@@ -16,8 +20,20 @@ class ModelEvaluation:
 
     def eval_metrics(self,actual, pred):
         acc_score = accuracy_score(actual, pred)
-        confusion = confusion_matrix(actual, pred)
-        return acc_score, confusion
+        precision = precision_score(actual, pred)
+        f1score = f1_score(actual, pred)
+        return acc_score, precision,f1score
+    
+    def track_mlflow(self,best_model,acc_score,precision,f1score):
+        with mlflow.start_run():
+            f1_score = f1score
+            precision_score = precision
+            accu_score = acc_score
+
+            mlflow.log_metric("accuracy_score",accu_score)
+            mlflow.log_metric("f1_score",f1_score)
+            mlflow.log_metric("precision_score",precision_score)
+            mlflow.sklearn.log_model(best_model,"model")
     
     def evaluate(self):
 
@@ -25,19 +41,27 @@ class ModelEvaluation:
         model = joblib.load(self.config.model_path)
 
         test_x = test_data.drop([self.config.target_column], axis=1)
-        test_y = test_data[[self.config.target_column]]
+        test_y = test_data[[self.config.target_column]].replace(-1,0)
 
-
+        lr = XGBClassifier()
 
         predicted_qualities = model.predict(test_x)
 
-        (acc_score, confusion) = self.eval_metrics(test_y, predicted_qualities)
+        (acc_score, precision,f1score) = self.eval_metrics(test_y, predicted_qualities)
+        self.track_mlflow(best_model=lr,acc_score=acc_score,f1score=f1score,precision=precision)
             
-            # Saving metrics as local
+        # Saving metrics as local
         scores = {"accuracy":acc_score }
         save_json(path=Path(self.config.metric_file_name), data=scores)
 
         return scores
+    
+
+# if __name__ == "__main__":
+#     config = ConfigurationManager()
+#     model_evaluate_configuration = config.get_model_evaluation_config()
+#     model_evaluate = ModelEvaluation(model_evaluate_configuration)
+#     print(model_evaluate.evaluate())
 
 
 
